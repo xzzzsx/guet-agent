3b452eb82550d293f6010a93ed8daf28f1ebfd7b
zeng-shaoxiong <3466202941@qq.com>
2025-07-08 14:04:53 +0800

    feat(chat): 优化项目选择功能并添加跨页面通信
    - 在项目列表页面添加选择项目的功能- 使用 BroadcastChannel 实现跨页面通信，通知聊天页面刷新
    - 保留原有的 window.opener.postMessage作为后备通信方式
    - 优化代码格式，提高可读性

--- guli-ui/src/views/chat/knowledge.vue
+++ guli-ui/src/views/chat/knowledge.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="项目" prop="type">
         <el-select
           v-model="queryParams.projectId"
@@ -11,7 +18,11 @@
           <el-option
             v-for="project in projectList"
             :key="project.projectId"
-            :label="project.projectId != undefined ? project.projectId + ' ' + project.projectName : project.projectName"
+            :label="
+              project.projectId != undefined
+                ? project.projectId + ' ' + project.projectName
+                : project.projectName
+            "
             :value="project.projectId"
           />
         </el-select>
@@ -25,13 +36,21 @@
         />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">查询</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+          size="mini"
+          @click="handleQuery"
+          >查询</el-button
+        >
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery"
+          >重置</el-button
+        >
       </el-form-item>
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-<!--      <el-col :span="1.5">
+      <!--      <el-col :span="1.5">
         <el-button
           type="primary"
           plain
@@ -41,7 +60,7 @@
           v-hasPermi="['system:post:add']"
         >新增</el-button>
       </el-col>-->
-<!--      <el-col :span="1.5">
+      <!--      <el-col :span="1.5">
         <el-button
           type="danger"
           plain
@@ -52,20 +71,36 @@
           v-hasPermi="['system:post:remove']"
         >删除</el-button>
       </el-col>-->
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar
+        :showSearch.sync="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="knowledges" @selection-change="handleSelectionChange">
+    <el-table
+      v-loading="loading"
+      :data="knowledges"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column label="知识文件编号" align="center" prop="knowledgeId" />
       <el-table-column label="上传用户" align="center" prop="createBy" />
       <el-table-column label="项目编号" align="center" prop="projectId" />
       <el-table-column label="文件名" align="center" prop="fileName" />
-      <el-table-column label="上传时间" align="center" prop="createTime" width="180">
+      <el-table-column
+        label="上传时间"
+        align="center"
+        prop="createTime"
+        width="180"
+      >
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding fixed-width"
+      >
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -73,30 +108,29 @@
             icon="el-icon-view"
             @click="handleView(scope.row)"
             v-hasPermi="['chat:knowledge:queryVo']"
-          >查看内容</el-button>
+            >查看内容</el-button
+          >
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['chat:knowledge:remove']"
-          >删除</el-button>
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       :page.sync="queryParams.pageNum"
       :limit.sync="queryParams.pageSize"
       @pagination="getList"
     />
 
-    <el-dialog
-      title="文件内容内下"
-      :visible.sync="dialogOpen"
-      width="550px">
+    <el-dialog title="文件内容内下" :visible.sync="dialogOpen" width="550px">
       <span>{{ fileContent }}</span>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="dialogOpen = false">OK</el-button>
@@ -107,9 +141,7 @@
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="选择项目">
-          <el-select
-            v-model="form.projectId"
-            style="width: 240px">
+          <el-select v-model="form.projectId" style="width: 240px">
             <el-option
               v-for="project in formProjectList"
               :key="project.projectId"
@@ -125,9 +157,12 @@
             :data="fileData"
             name="file"
             :limit="1"
-            :file-list="fileList">
+            :file-list="fileList"
+          >
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传txt、word、pdf文件，且不超过5M</div>
+            <div slot="tip" class="el-upload__tip">
+              只能上传txt、word、pdf文件，且不超过5M
+            </div>
           </el-upload>
         </el-form-item>
       </el-form>
@@ -140,18 +175,24 @@
 </template>
 
 <script>
-import { listKnowledge, getKnowledge, addKnowledge, listProject, delKnowledge} from "@/api/chat/knowledge";
+import {
+  listKnowledge,
+  getKnowledge,
+  addKnowledge,
+  listProject,
+  delKnowledge,
+} from "@/api/chat/knowledge";
 
 export default {
   name: "knowledge",
-  dicts: ['sys_normal_disable'],
+  dicts: ["sys_normal_disable"],
   data() {
     return {
       projectList: [],
       formProjectList: [],
-      fileList: [{fileName: 'food.jpeg', url: ""}],
+      fileList: [{ fileName: "food.jpeg", url: "" }],
       fileData: {
-        projectId: undefined
+        projectId: undefined,
       },
       dialogOpen: false,
       fileContent: "",
@@ -178,21 +219,21 @@ export default {
         pageNum: 1,
         pageSize: 10,
         projectId: undefined,
-        user: undefined
+        user: undefined,
       },
       // 表单参数
       form: {
-        projectId: 109
+        projectId: 109,
       },
       // 表单校验
       rules: {
         postName: [
-          { required: true, message: "岗位名称不能为空", trigger: "blur" }
+          { required: true, message: "岗位名称不能为空", trigger: "blur" },
         ],
         postCode: [
-          { required: true, message: "岗位编码不能为空", trigger: "blur" }
-        ]
-      }
+          { required: true, message: "岗位编码不能为空", trigger: "blur" },
+        ],
+      },
     };
   },
   created() {
@@ -201,19 +242,22 @@ export default {
   },
   methods: {
     getProjectList() {
-      listProject().then(response => {
+      listProject().then((response) => {
         this.formProjectList = response.rows;
         this.projectList = Array.of({
-          "projectId": undefined,
-          "projectName": "全部"
+          projectId: undefined,
+          projectName: "全部",
         }).concat(response.rows);
-        this.form.projectId = this.formProjectList.length == 0 ? undefined : this.formProjectList[0].projectId
+        this.form.projectId =
+          this.formProjectList.length == 0
+            ? undefined
+            : this.formProjectList[0].projectId;
       });
     },
     /** 查询岗位列表 */
     getList() {
       this.loading = true;
-      listKnowledge(this.queryParams).then(response => {
+      listKnowledge(this.queryParams).then((response) => {
         this.knowledges = response.rows;
         this.total = response.total;
         this.loading = false;
@@ -231,7 +275,7 @@ export default {
         postCode: undefined,
         postName: undefined,
         status: "0",
-        remark: undefined
+        remark: undefined,
       };
       this.resetForm("form");
     },
@@ -240,11 +284,11 @@ export default {
       this.queryParams.pageNum = 1;
       this.getList();
     },
-    handleView(row){
+    handleView(row) {
       const knowledgeId = row.knowledgeId;
-      console.log(knowledgeId)
-      getKnowledge(knowledgeId).then(response => {
-        this.fileContent = response.data.content
+      console.log(knowledgeId);
+      getKnowledge(knowledgeId).then((response) => {
+        this.fileContent = response.data.content;
         this.dialogOpen = true;
       });
     },
@@ -256,9 +300,9 @@ export default {
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.postId)
-      this.single = selection.length!=1
-      this.multiple = !selection.length
+      this.ids = selection.map((item) => item.postId);
+      this.single = selection.length != 1;
+      this.multiple = !selection.length;
     },
     /** 新增按钮操作 */
     handleAdd() {
@@ -267,28 +311,32 @@ export default {
       this.title = "上传知识库";
     },
     /** 提交按钮 */
-    submitForm: function() {
-      this.$refs["form"].validate(valid => {
+    submitForm: function () {
+      this.$refs["form"].validate((valid) => {
         if (valid) {
-            addPost(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
+          addPost(this.form).then((response) => {
+            this.$modal.msgSuccess("新增成功");
+            this.open = false;
+            this.getList();
+          });
         }
       });
     },
     /** 删除按钮操作 */
     handleDelete(row) {
-      console.log(row)
+      console.log(row);
       const knowledgeId = row.knowledgeId;
-      this.$modal.confirm('是否确认删除知识库编号为"' + knowledgeId + '"的数据项？').then(function() {
-        return delKnowledge(knowledgeId);
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
-    }
+      this.$modal
+        .confirm('是否确认删除知识库编号为"' + knowledgeId + '"的数据项？')
+        .then(function () {
+          return delKnowledge(knowledgeId);
+        })
+        .then(() => {
+          this.getList();
+          this.$modal.msgSuccess("删除成功");
+        })
+        .catch(() => {});
+    },
   },
 };
 </script>
--- guli-ui/src/views/chat/project.vue
+++ guli-ui/src/views/chat/project.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="项目名称" prop="projectName">
         <el-input
           v-model="queryParams.projectName"
@@ -25,8 +32,16 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+          size="mini"
+          @click="handleQuery"
+          >搜索</el-button
+        >
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery"
+          >重置</el-button
+        >
       </el-form-item>
     </el-form>
 
@@ -39,7 +54,8 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['system:post:add']"
-        >新增</el-button>
+          >新增</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -50,7 +66,8 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['chat:project:edit']"
-        >修改</el-button>
+          >修改</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -61,23 +78,41 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['chat:project:remove']"
-        >删除</el-button>
+          >删除</el-button
+        >
       </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar
+        :showSearch.sync="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="projectList" @selection-change="handleSelectionChange">
+    <el-table
+      v-loading="loading"
+      :data="projectList"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="项目编号" align="center" prop="projectId" />
       <el-table-column label="项目名称" align="center" prop="projectName" />
       <el-table-column label="模型类型" align="center" prop="type" />
       <el-table-column label="具体模型" align="center" prop="model" />
-      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+      <el-table-column
+        label="创建时间"
+        align="center"
+        prop="createTime"
+        width="180"
+      >
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding" width="280">
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding"
+        width="280"
+      >
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -85,27 +120,38 @@
             icon="el-icon-edit"
             @click="handleUpload(scope.row)"
             v-hasPermi="['system:knowledge:add']"
-          >上传知识库</el-button>
+            >上传知识库</el-button
+          >
           <el-button
             size="mini"
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['system:project:edit']"
-          >修改</el-button>
+            >修改</el-button
+          >
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['system:project:remove']"
-          >删除</el-button>
+            >删除</el-button
+          >
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-check"
+            @click="handleSelect(scope.row)"
+            v-hasPermi="['system:project:select']"
+            >选择</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       :page.sync="queryParams.pageNum"
       :limit.sync="queryParams.pageSize"
@@ -113,7 +159,12 @@
     />
 
     <!-- 添加或修改项目对话框 -->
-    <el-dialog :title="projectTitle" :visible.sync="projectOpen" width="500px" append-to-body>
+    <el-dialog
+      :title="projectTitle"
+      :visible.sync="projectOpen"
+      width="500px"
+      append-to-body
+    >
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="项目名称" prop="projectName">
           <el-input v-model="form.projectName" placeholder="请输入项目名称" />
@@ -125,7 +176,8 @@
               :key="mdl"
               :label="mdl"
               :value="mdl"
-            >{{mdl}}</el-radio>
+              >{{ mdl }}</el-radio
+            >
           </el-radio-group>
         </el-form-item>
       </el-form>
@@ -135,7 +187,12 @@
       </div>
     </el-dialog>
 
-    <el-dialog title="上传知识库" :visible.sync="acknowledgeOpen" width="550px" append-to-body>
+    <el-dialog
+      title="上传知识库"
+      :visible.sync="acknowledgeOpen"
+      width="550px"
+      append-to-body
+    >
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="文件上传" prop="fileUpload">
           <el-upload
@@ -147,9 +204,12 @@
             :before-remove="removeFile"
             name="file"
             multiple
-            :file-list="fileList">
+            :file-list="fileList"
+          >
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传txt、word、pdf文件，如果需要查看文件详情，请移步知识库管理</div>
+            <div slot="tip" class="el-upload__tip">
+              只能上传txt、word、pdf文件，如果需要查看文件详情，请移步知识库管理
+            </div>
           </el-upload>
         </el-form-item>
       </el-form>
@@ -161,22 +221,27 @@
 </template>
 
 <script>
-import {listProject, getProject, delProject, addProject, updateProject, listAcknowledges, removeFile} from "@/api/chat/project";
+import {
+  listProject,
+  getProject,
+  delProject,
+  addProject,
+  updateProject,
+  listAcknowledges,
+  removeFile,
+} from "@/api/chat/project";
 import { getToken } from "@/utils/auth";
 
 export default {
   name: "Post",
-  dicts: ['sys_normal_disable'],
+  dicts: ["sys_normal_disable"],
   data() {
     return {
-      models: [
-        "openai qwen",
-        "ollama qwen2.5:1.5b"
-      ],
+      models: ["openai qwen", "ollama qwen2.5:1.5b"],
       fileList: [],
       fileData: {},
-      fileUploadUrl: process.env.VUE_APP_BASE_API + '/chat/knowledge/upload',
-      fileUploadHeaders: {Authorization: 'Bearer ' + getToken()},
+      fileUploadUrl: process.env.VUE_APP_BASE_API + "/chat/knowledge/upload",
+      fileUploadHeaders: { Authorization: "Bearer " + getToken() },
       // 遮罩层
       loading: true,
       // 选中数组
@@ -204,19 +269,19 @@ export default {
         pageSize: 10,
         projectName: undefined,
         type: undefined,
-        model: undefined
+        model: undefined,
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
         projectName: [
-          { required: true, message: "项目名称不能为空", trigger: "blur" }
+          { required: true, message: "项目名称不能为空", trigger: "blur" },
         ],
         selectedModel: [
-          { required: true, message: "请选择模型", trigger: "blur" }
-        ]
-      }
+          { required: true, message: "请选择模型", trigger: "blur" },
+        ],
+      },
     };
   },
   created() {
@@ -224,7 +289,7 @@ export default {
   },
   watch: {
     selectedModel(n, o) {
-      if (n != undefined){
+      if (n != undefined) {
         let mt = n.split(" ");
         this.queryParams.type = mt[0];
         this.queryParams.model = mt[1];
@@ -237,8 +302,8 @@ export default {
       immediate: true,
       deep: true,
       handler(n, o) {
-        let slctModel = n.selectedModel
-        if (slctModel != undefined){
+        let slctModel = n.selectedModel;
+        if (slctModel != undefined) {
           let mt = slctModel.split(" ");
           this.form.type = mt[0];
           this.form.model = mt[1];
@@ -246,14 +311,14 @@ export default {
           this.form.type = undefined;
           this.form.model = undefined;
         }
-      }
-    }
+      },
+    },
   },
   methods: {
     /** 查询项目列表 */
     getList() {
       this.loading = true;
-      listProject(this.queryParams).then(response => {
+      listProject(this.queryParams).then((response) => {
         this.projectList = response.rows;
         this.total = response.total;
         this.loading = false;
@@ -270,7 +335,7 @@ export default {
         projectId: undefined,
         selectedModel: this.models[0],
         type: undefined,
-        model: undefined
+        model: undefined,
       };
       this.resetForm("form");
     },
@@ -287,9 +352,35 @@ export default {
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.projectId)
-      this.single = selection.length!=1
-      this.multiple = !selection.length
+      this.ids = selection.map((item) => item.projectId);
+      this.single = selection.length != 1;
+      this.multiple = !selection.length;
+    },
+    // 选择项目
+    async handleSelect(row) {
+      console.log("开始处理项目选择，项目ID:", row.projectId);
+      const projectIdStr = String(row.projectId);
+      localStorage.setItem("currentProjectId", projectIdStr);
+      console.log("已将项目ID存入localStorage:", projectIdStr);
+      this.$message.success("已选择项目: " + row.projectName);
+
+      // 使用BroadcastChannel通知聊天页面刷新
+      const channel = new BroadcastChannel("project_channel");
+      const message = {
+        type: "projectChanged",
+        projectId: Number(row.projectId),
+        timestamp: Date.now(),
+      };
+      console.log("准备通过BroadcastChannel发送消息:", message);
+      channel.postMessage(message);
+      console.log("BroadcastChannel消息已发送");
+      channel.close();
+
+      // 保留原有window.opener.postMessage作为后备
+      if (window.opener) {
+        console.log("通过window.opener.postMessage发送消息:", message);
+        window.opener.postMessage(message, "*");
+      }
     },
     /** 新增按钮操作 */
     handleAdd() {
@@ -300,8 +391,8 @@ export default {
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
-      const projectId = row.projectId || this.ids
-      getProject(projectId).then(response => {
+      const projectId = row.projectId || this.ids;
+      getProject(projectId).then((response) => {
         this.form = response.data;
         this.form.selectedModel = this.form.type + " " + this.form.model;
         this.projectOpen = true;
@@ -309,23 +400,26 @@ export default {
       });
     },
     handleUpload(row) {
-      this.fileData.projectId = row.projectId || this.ids
-      listAcknowledges(this.fileData).then(response => {
+      this.fileData.projectId = row.projectId || this.ids;
+      listAcknowledges(this.fileData).then((response) => {
         this.form = response.rows;
         this.acknowledgeOpen = true;
       });
     },
-    handleSuccess(res, file){
+    handleSuccess(res, file) {
       if (res.code === 200) {
-        this.fileList.push({ id: res.data, name: file.name});
+        this.fileList.push({ id: res.data, name: file.name });
         this.$modal.msgSuccess("文件上传成功");
       } else {
         this.$modal.msgSuccess("文件上传失败");
       }
     },
-    removeFile(file, fileList){
-      let removeFileData = {knowledgeId: file.id, projectId: this.fileData.projectId};
-      removeFile(removeFileData).then(response => {
+    removeFile(file, fileList) {
+      let removeFileData = {
+        knowledgeId: file.id,
+        projectId: this.fileData.projectId,
+      };
+      removeFile(removeFileData).then((response) => {
         if (response.code === 200) {
           this.fileList = fileList;
           return true;
@@ -333,7 +427,7 @@ export default {
         return false;
       });
     },
-    closeForm(){
+    closeForm() {
       this.fileList = [];
       this.acknowledgeOpen = false;
     },
@@ -344,17 +438,17 @@ export default {
     //   });
     // },
     /** 提交按钮 */
-    submitForm: function() {
-      this.$refs["form"].validate(valid => {
+    submitForm: function () {
+      this.$refs["form"].validate((valid) => {
         if (valid) {
           if (this.form.projectId != undefined) {
-            updateProject(this.form).then(response => {
+            updateProject(this.form).then((response) => {
               this.$modal.msgSuccess("修改成功");
               this.projectOpen = false;
               this.getList();
             });
           } else {
-            addProject(this.form).then(response => {
+            addProject(this.form).then((response) => {
               this.$modal.msgSuccess("新增成功");
               this.projectOpen = false;
               this.getList();
@@ -366,13 +460,17 @@ export default {
     /** 删除按钮操作 */
     handleDelete(row) {
       const projectIds = row.projectId || this.ids;
-      this.$modal.confirm('是否确认删除项目编号为"' + projectIds + '"的数据项？').then(function() {
-        return delProject(projectIds);
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
-    }
-  }
+      this.$modal
+        .confirm('是否确认删除项目编号为"' + projectIds + '"的数据项？')
+        .then(function () {
+          return delProject(projectIds);
+        })
+        .then(() => {
+          this.getList();
+          this.$modal.msgSuccess("删除成功");
+        })
+        .catch(() => {});
+    },
+  },
 };
 </script>
