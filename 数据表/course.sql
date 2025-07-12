419cf08aa63a0071a0050b6b303f1d747ca24e79
zeng-shaoxiong <3466202941@qq.com>
2025-07-12 14:30:28 +0800

    docs(RAG): 添加桂林电子科技大学北海校区知识库
    - 新增桂林电子科技大学北海校区的知识库文档，包含校区分布、教学楼、宿舍、饮食、运动设施等信息
    - 添加多个MongoDB聊天记录文件，记录AI与用户的聊天数据

--- 数据表/course.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (1, '网络工程', 3, '计算机工程学院', 7400, 58);
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (2, '物流管理', 2, '经济与管理学院', 4000, 98);
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (3, '机械工程', 1, '海洋工程学院', 4500, 100);
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (4, '电信工程及管理', 2, '电子信息学院', 4200, 102);
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (5, '大数据管理与应用', 4, '计算机工程学院', 5700, 68);
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (6, '工业设计', 1, '设计与创意学院', 4000, 61);
-INSERT INTO `course` (`id`, `name`, `edu`, `type`, `price`, `duration`) VALUES (7, '网络工程(NIIT)', 2, '计算机工程学院', 12500, 66);
--- 数据表/school.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `school` (`id`, `name`, `city`) VALUES (1, '北海校区', '北海市银海区南珠大道9号');
-INSERT INTO `school` (`id`, `name`, `city`) VALUES (2, '花江校区', '桂林市灵川县灵田镇');
-INSERT INTO `school` (`id`, `name`, `city`) VALUES (3, '金鸡岭校区', '桂林市七星区金鸡路1号');
-INSERT INTO `school` (`id`, `name`, `city`) VALUES (4, '六合路校区', '桂林市七星区六合路123号');
